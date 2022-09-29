b77532df8a3f17e20253668367b2006a0757b8d3
djeong20 <54725479+djeong20@users.noreply.github.com>
2022-09-29 16:09:30 -0500

    Time: 4 ms (58.08%), Space: 9.1 MB (30.49%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -19,18 +19,13 @@ public:
         }
         
         while (true) {
-            cout << "i1: " << i1 << endl;
-            cout << "i2: " << i2 << endl;
             if (i1 == nums1.size()) {
-                cout << "more to push" << endl;
                 while (i2 < n) {
-                    cout << "push_back: " << nums2[i2] << endl;
                     nums1.push_back(nums2[i2]);
                     i2++;
                 }
                 break;
             }
-            
             
             if (nums1[i1] >= nums2[i2]) {
                 nums1.insert(nums1.begin()+i1, nums2[i2]);
@@ -41,7 +36,6 @@ public:
             }
             
             if (i2 == nums2.size()) {
-                cout << "end to push" << endl;
                 break;
             }
             

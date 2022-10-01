fa1393ab90288af4888d1faaf8a411ebc1d4473a
djeong20 <54725479+djeong20@users.noreply.github.com>
2022-10-01 11:14:39 -0500

    Time: 173 ms (5.21%), Space: 19.7 MB (14.38%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,31 @@
+#include <unordered_map>
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        std::unordered_map<int, int> dict;
+        
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            int val = nums[i];
+            
+            std::cout << val << std::endl;
+            auto itr = dict.find(val);
+            
+            if (itr == dict.end()) {
+                dict.insert({val, 1});
+            }
+            else {
+                itr->second += 1;
+            }
+            itr = dict.find(val);
+            if (itr->second > n/2) {
+                return itr->first;
+            }
+            
+            
+        }
+        return 0;
+    }
+};

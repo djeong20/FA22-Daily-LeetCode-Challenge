298ce17d8092722599ae3e1069585613eefb8cf7
djeong20 <54725479+djeong20@users.noreply.github.com>
2022-10-03 01:40:16 -0500

    Time: 0 ms (100.00%), Space: 10.3 MB (6.68%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -12,20 +12,50 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == nullptr && q == nullptr) {
+        std::queue<TreeNode*> p_queue;
+        std::queue<TreeNode*> q_queue;
+        
+        if (p != nullptr) {
+            p_queue.push(p);
+            
+        } 
+        
+        if (q != nullptr) {
+            q_queue.push(q);
+        }
+        
+        
+        while (!p_queue.empty() && !q_queue.empty()) {
+            TreeNode *curr_p = p_queue.front();
+            TreeNode *curr_q = q_queue.front();
+            
+            if (curr_p->val != curr_q->val) {
+                return false;
+            }
+            
+            if (curr_p->left != nullptr && curr_q->left != nullptr) {
+                p_queue.push(curr_p->left);
+                q_queue.push(curr_q->left);
+            } 
+            else if (curr_p->left != curr_q->left) {
+                return false;
+            }
+            if (curr_p->right != nullptr && curr_q->right != nullptr) {
+                p_queue.push(curr_p->right);
+                q_queue.push(curr_q->right);
+            }
+            else if (curr_p->right != curr_q->right) {
+                return false;
+            }
+            
+            p_queue.pop();
+            q_queue.pop();
+        }
+        
+        if (p_queue.empty() && q_queue.empty()) {
             return true;
         }
-        else if (p == nullptr && q != nullptr) {
-            return false;
-        }
-        else if (p != nullptr && q == nullptr) {
-            return false;
-        }
         
-        if (p->val != q->val) {
-            return false;
-        }
-        
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        return false;
     }
 };

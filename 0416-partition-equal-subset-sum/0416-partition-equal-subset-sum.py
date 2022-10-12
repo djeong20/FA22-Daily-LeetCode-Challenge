5bebc04dd19a4cff81dd7a8f27092cbaf5abe33b
djeong20 <54725479+djeong20@users.noreply.github.com>
2022-10-11 20:53:44 -0500

    Time: 1171 ms (75.66%), Space: 13.9 MB (97.42%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,4 +1,3 @@
-
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         s = sum(nums)
@@ -17,3 +16,15 @@ class Solution:
                     dp[i] = dp[i] or dp[i-num]
 
         return dp[target]
+    
+#  0  1. 2. 3. 4. 5. 6. 7. 8. 9  10. 11     
+# [T, F, F, F, F, F, F, F, F, F, F, F]
+
+# num: 1
+# [T, T, F, F, F, F, F, F, F, F, F, F]
+
+# num: 5
+# [T, T, F, F, F, T, T, F, F, F, F, F]
+
+# num: 11
+# [T, T, F, F, F, T, T, F, F, F, F, T]

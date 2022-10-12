b7efc6c96f234858ff4d855a1453caafceb5c153
djeong20 <54725479+djeong20@users.noreply.github.com>
2022-10-11 21:59:08 -0500

    Time: 1022 ms (10.06%), Space: 17.2 MB (36.09%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, INT_MAX-1);
+        
+        dp[0] = 0;
+        
+        for (int i = 0; i < n; i++) {
+            // update jump index
+            for (int j = 1; j <= nums[i] and (i+j) < n; j++) {
+                dp[i+j] = min(dp[i+j], dp[i]+1);
+            }
+        }
+        return dp[n-1];
+    }
+    
+    
+    // [2, 3, 1, 1, 4]
+    
+    // i = 0
+    // [0, m, m, m, m]
+    // [0, 1, 1, m, m]
+    
+    // i = 1
+    // [0, 1, 1, 2, 2]
+    
+};

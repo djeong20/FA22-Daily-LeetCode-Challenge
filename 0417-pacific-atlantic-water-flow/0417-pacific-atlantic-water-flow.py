7637628e371aeffed5e4dc574bf631d5cca361c9
djeong20 <54725479+djeong20@users.noreply.github.com>
2022-10-30 01:19:55 -0500

    Time: 276 ms (97.28%), Space: 15.5 MB (95.34%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,50 +1,42 @@
 class Solution:
-    # 0 0 0 : None
-    # 0 0 1 : Pacific
-    # 0 1 0 : Atlantic
-    # 0 1 1 : Both
+    # dfs from edges (pacific ocean, atlantic)
     
-    
-    def dfs(self, heights, r, c,  visited, d):
-        if (r,c) in d:
-            return d[(r,c)]
-        
-        direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    def dfs(self, heights, r, c, visited):
         m, n = len(heights), len(heights[0])
-        res = 0
         
-        if r == 0: res = res | 1
-        if r == m-1: res = res | 2
-        if c == 0: res = res | 1
-        if c == n-1: res = res | 2
-            
-        if res == 3: return res
-        
-        visited.add((r,c))
+        visited[r][c] = True
         h = heights[r][c]
         
-        for dr, dc in direction:
-            x, y = r+dr, c+dc
+        for dx, dy in self.dir:
+            x, y = r + dx, c + dy
             
-            if x < 0 or x >= m or y < 0 or y >= n or h < heights[x][y] or (x,y) in visited:
-                continue
+            # if visited, not in range, lower (cant move)
+            if 0 <= x < m and 0 <= y < n and not visited[x][y] and heights[x][y] >= h:
+                self.dfs(heights, x, y, visited)
             
-            res = res | self.dfs(heights, x, y, visited, d)
-        
-        return res
     
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         m, n = len(heights), len(heights[0])
         res = []
         
-        d = dict()
+        self.dir = [(-1,0), (1,0), (0,-1), (0,1)]
+        pacific = [[False] * n for _ in range(m)]
+        atlantic = [[False] * n for _ in range(m)]
+        
+        # traverse edges
+        for i in range(n):
+            self.dfs(heights, 0, i, pacific)
+            
+            self.dfs(heights, m-1, i, atlantic)
+            
+        for i in range(m):
+            self.dfs(heights, i, 0, pacific)
+            
+            self.dfs(heights, i, n-1, atlantic)
         
         for r in range(m):
             for c in range(n):
-                
-                d[(r,c)] = self.dfs(heights, r, c, set(), d)
-                
-                if d[(r,c)] == 3:
+                if pacific[r][c] and atlantic[r][c]:
                     res.append([r,c])
                     
         return res
